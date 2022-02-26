ac166f70adcba9ab7604a6c31ab46c55b28c4d61
charswang <1043594828@qq.com>
2022-02-26 08:40:53 +0800

    update 每日一题 2022-2-26

--- night/src/main/java/leetcode/P2016.java
+++ night/src/main/java/leetcode/P2016.java
@@ -9,16 +9,17 @@ public class P2016 {
     public int maximumDifference(int[] nums) {
         int res = -1;
         int min = nums[0];
-        int max = res;
+        // int max = res;
         for (int i = 1;i < nums.length;i++){
             if (nums[i]>min){
-                res = nums[i] - min;
-                max = Math.max(res,max);
+                res = Math.max(res,nums[i] - min);
+                // res = nums[i] - min;
+                // max = Math.max(res,max);
             }else {
                 min = Math.min(min,nums[i]);
             }
 
         }
-        return max;
+        return res;
     }
 }
